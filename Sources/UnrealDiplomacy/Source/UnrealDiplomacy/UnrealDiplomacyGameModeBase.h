bced3e04f49d5516919165cb492f594af6412c79
Miroslav Valach <skvalach@gmail.com>
2022-02-26 22:48:06 +0100

    Trying to establish minimal working multiplayer + getting familiar with UE 28h

---
+++ Sources/UnrealDiplomacy/Source/UnrealDiplomacy/UnrealDiplomacyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealDiplomacyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALDIPLOMACY_API AUnrealDiplomacyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
